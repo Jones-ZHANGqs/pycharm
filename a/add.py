68f5cca34e715daa9097d6b74db6622dd3b75d26
Jones <2821576087@qq.com>
2023-11-08 09:42:31 +0800

    112

---
+++ a/add.py
@@ -0,0 +1,18 @@
+import warnings
+# from memory_profiler import profile
+
+import requests
+import web
+
+# !111
+
+warnings.filterwarnings("ignore")
+
+urls = (
+    "/ai/v1/load_time_elasticity", "load_time_elasticity"
+)
+
+app = web.application(urls, globals())
+# _author:"zqs"
+#_author:"zqs"
+#date:2023/11/7
